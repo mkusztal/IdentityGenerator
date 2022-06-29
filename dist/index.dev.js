426c7cb89c050a3a71bdb94b84526ff01fb1c0ce
Maciej Kusztal <maciej.kusztal1995@gmail.com>
2022-06-29 23:24:09 +0300

    Create file works

--- dist/index.dev.js
+++ dist/index.dev.js
@@ -2,39 +2,42 @@
 
 var fs = require('fs');
 
-fs.writeFile('people.JSON', data, function (err) {
-  if (err) throw err;
-  console.log('File has been successfully generated! Check people.json');
-  var genderArr = ['male', 'female'];
-  var maleNamesArr = ['Igor', 'Robert', 'Maksymilian', 'Aleksander', 'Krystian', 'Artur', 'Maciej', 'Paweł', 'Rafał', 'Marcin'];
-  var femaleNamesArr = ['Aleksandra', 'Alina', 'Sandra', 'Klaudia', 'Weronika', 'Adrianna', 'Julia', 'Karolina', 'Maja', 'Monika'];
-  var lastNamesArr = ['Kowalski', 'Malinowski', 'Krab', 'Chyba', 'Wagner', 'Klose', 'Sikora'];
-  var people = [];
+var genderArr = ['male', 'female'];
+var maleNamesArr = ['Igor', 'Robert', 'Maksymilian', 'Aleksander', 'Krystian', 'Artur', 'Maciej', 'Paweł', 'Rafał', 'Marcin'];
+var femaleNamesArr = ['Aleksandra', 'Alina', 'Sandra', 'Klaudia', 'Weronika', 'Adrianna', 'Julia', 'Karolina', 'Maja', 'Monika'];
+var lastNamesArr = ['Kowalski', 'Malinowski', 'Krab', 'Chyba', 'Wagner', 'Klose', 'Sikora'];
+var people = [];
 
-  var randChoice = function randChoice(arr) {
-    var randomIndex = Math.floor(Math.random() * arr);
-    var item = arr[randomIndex];
-    return item;
-  };
+var randChoice = function randChoice(arr) {
+  var randomIndex = Math.floor(Math.random() * arr);
+  var item = arr[randomIndex];
+  return item;
+};
 
-  for (var i = 0; i < 20; i++) {
-    var genderChoice = randChoice(genderArr);
+for (var i = 0; i < 20; i++) {
+  var genderChoice = randChoice(genderArr); // nie pojawia się
+  // nie ma prawa pojawić się imię bo go nie przypisałem
 
-    if (genderChoice === 'male') {
-      var maleNamesChoice = randChoice(maleNamesArr);
-    } else if (genderChoice === 'female') {
-      var femaleNamesChoice = randChoice(femaleNamesArr);
-    }
-
-    var randomLastName = Math.floor(Math.random() * lastNamesArr);
-    var randomAge = Math.floor(Math.random() * 78) + 18;
-    var person = {
-      gender: genderChoice,
-      lastName: randomLastName,
-      age: randomAge
-    };
-    people.push(person);
+  if (genderChoice === 'male') {
+    var maleNamesChoice = randChoice(maleNamesArr);
+  } else if (genderChoice === 'female') {
+    var femaleNamesChoice = randChoice(femaleNamesArr);
   }
 
-  var peopleFile = JSON.stringify(people);
+  var randomLastName = Math.floor(Math.random() * lastNamesArr); // null
+
+  var randomAge = Math.floor(Math.random() * 78) + 18; // poprawnie
+
+  var person = {
+    gender: genderChoice,
+    lastName: randomLastName,
+    age: randomAge
+  };
+  people.push(person);
+}
+
+var peopleFile = JSON.stringify(people);
+fs.writeFile('people.JSON', peopleFile, function (err) {
+  if (err) throw err;
+  console.log('File has been successfully generated! Check people.json');
 });
--- index.js
+++ index.js
@@ -1,72 +1,73 @@
 const fs = require('fs');
 
-fs.writeFile('people.JSON', data, (err) => {
-  if (err) throw err;
-  console.log('File has been successfully generated! Check people.json');
+const genderArr = ['male', 'female'];
+const maleNamesArr = [
+  'Igor',
+  'Robert',
+  'Maksymilian',
+  'Aleksander',
+  'Krystian',
+  'Artur',
+  'Maciej',
+  'Paweł',
+  'Rafał',
+  'Marcin',
+];
+const femaleNamesArr = [
+  'Aleksandra',
+  'Alina',
+  'Sandra',
+  'Klaudia',
+  'Weronika',
+  'Adrianna',
+  'Julia',
+  'Karolina',
+  'Maja',
+  'Monika',
+];
+const lastNamesArr = [
+  'Kowalski',
+  'Malinowski',
+  'Krab',
+  'Chyba',
+  'Wagner',
+  'Klose',
+  'Sikora',
+];
 
-  const genderArr = ['male', 'female'];
-  const maleNamesArr = [
-    'Igor',
-    'Robert',
-    'Maksymilian',
-    'Aleksander',
-    'Krystian',
-    'Artur',
-    'Maciej',
-    'Paweł',
-    'Rafał',
-    'Marcin',
-  ];
-  const femaleNamesArr = [
-    'Aleksandra',
-    'Alina',
-    'Sandra',
-    'Klaudia',
-    'Weronika',
-    'Adrianna',
-    'Julia',
-    'Karolina',
-    'Maja',
-    'Monika',
-  ];
-  const lastNamesArr = [
-    'Kowalski',
-    'Malinowski',
-    'Krab',
-    'Chyba',
-    'Wagner',
-    'Klose',
-    'Sikora',
-  ];
+const people = [];
+const randChoice = (arr) => {
+  const randomIndex = Math.floor(Math.random() * arr);
 
-  const people = [];
-  const randChoice = (arr) => {
-    const randomIndex = Math.floor(Math.random() * arr);
+  const item = arr[randomIndex];
+  return item;
+};
 
-    const item = arr[randomIndex];
-    return item;
-  };
+for (let i = 0; i < 20; i++) {
+  const genderChoice = randChoice(genderArr); // nie pojawia się
 
-  for (let i = 0; i < 20; i++) {
-    const genderChoice = randChoice(genderArr);
-
-    if (genderChoice === 'male') {
-      const maleNamesChoice = randChoice(maleNamesArr);
-    } else if (genderChoice === 'female') {
-      const femaleNamesChoice = randChoice(femaleNamesArr);
-    }
-
-    const randomLastName = Math.floor(Math.random() * lastNamesArr);
-    const randomAge = Math.floor(Math.random() * 78) + 18;
-
-    const person = {
-      gender: genderChoice,
-      lastName: randomLastName,
-      age: randomAge,
-    };
-
-    people.push(person);
+  // nie ma prawa pojawić się imię bo go nie przypisałem
+  if (genderChoice === 'male') {
+    const maleNamesChoice = randChoice(maleNamesArr);
+  } else if (genderChoice === 'female') {
+    const femaleNamesChoice = randChoice(femaleNamesArr);
   }
 
-  const peopleFile = JSON.stringify(people);
+  const randomLastName = Math.floor(Math.random() * lastNamesArr); // null
+  const randomAge = Math.floor(Math.random() * 78) + 18; // poprawnie
+
+  const person = {
+    gender: genderChoice,
+    lastName: randomLastName,
+    age: randomAge,
+  };
+
+  people.push(person);
+}
+
+const peopleFile = JSON.stringify(people);
+
+fs.writeFile('people.JSON', peopleFile, (err) => {
+  if (err) throw err;
+  console.log('File has been successfully generated! Check people.json');
 });
